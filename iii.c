5361e16ce95284f664aeb56031b320df3cdeca1e
Devieswaran <devi.eswaran98@gmail.com>
2017-07-28 12:35:56 +0530

    Create iii.c

---
+++ iii.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("\n enter a year");
+scanf("%d",&a);
+if(a%4==0)
+{
+printf("\n leap year");
+}
+else
+{
+printf("\n is not leap year");
+}
+return 0;
+}
